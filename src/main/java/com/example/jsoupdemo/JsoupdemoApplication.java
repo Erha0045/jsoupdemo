3c9b02ad2ff99e44ba09aa7e2c106b3b6b693807
Erhan B <64098716+Erha0045@users.noreply.github.com>
2021-05-07 14:06:05 +0200

    Initial commit

---
+++ src/main/java/com/example/jsoupdemo/JsoupdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.jsoupdemo;
+
+import org.jsoup.nodes.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoupdemoApplication.class, args);
+    }
+
+}
